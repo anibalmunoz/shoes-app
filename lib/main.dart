ca0327ad259b27d9310e9e4fdb32c08665d6fc73
anibalmunoz <munoz4hernandez@gmail.com>
2023-01-27 18:25:06 -0600

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Shoes App',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Material App Bar'),
